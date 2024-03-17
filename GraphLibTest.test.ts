1496fa3763f28cb488998d99a3fc4eaafade23cc
Matthew Negasi <negasim@spu.edu>
2024-03-17 00:48:37 -0700

    Dijkstra, Kruskal, and Prims Algorithm added to the Library.

--- GraphLibTest.test.ts
+++ GraphLibTest.test.ts
@@ -1,6 +1,6 @@
 import Graph from "./GraphLibrary";
 
-describe("Graph Library", () => {
+describe("Graph Library Tests", () => {
   let graph: Graph;
 
   beforeEach(() => {
@@ -45,7 +45,7 @@ describe("Graph Library", () => {
     graph.addEdge("E", "F", 1);
     graph.addEdge("D", "F", 1);
 
-    const visited = [] as string[];
+    const visited = {} as { [key: string]: boolean };
     graph.DFS("A", visited);
     expect(Object.keys(visited)).toEqual(["A", "B", "E", "D", "F", "C"]);
   });
@@ -70,5 +70,183 @@ describe("Graph Library", () => {
     graph.BFS("A", visited);
 
     expect(Object.keys(visited)).toEqual(["A", "B", "C", "E", "F", "D"]);
+  });
+
+  it("Should perform Dijkstra's Algorithm from 1 vertex to another", () => {
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+    graph.addVertex("D");
+    graph.addVertex("E");
+    graph.addVertex("F");
+    graph.addVertex("J");
+
+    graph.addEdge("A", "B", 26);
+    graph.addEdge("A", "C", 2);
+    graph.addEdge("B", "E", 3);
+    graph.addEdge("C", "F", 8);
+    graph.addEdge("E", "D", 17);
+    graph.addEdge("E", "F", 1);
+    graph.addEdge("D", "F", 1);
+    graph.addEdge("J", "D", 5);
+
+    // Remember here that the edges are being set from the 1st parameter edge to the 2nd.
+    // This is important to consider when testing.
+
+    const result = graph.Dijkstra("A", "F");
+    expect(result).toEqual(10);
+  });
+
+  it("Should perform Dijkstra's Algorithm from 1 vertex to all", () => {
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+    graph.addVertex("D");
+    graph.addVertex("E");
+    graph.addVertex("F");
+    graph.addVertex("J");
+
+    graph.addEdge("A", "B", 26);
+    graph.addEdge("A", "C", 2);
+    graph.addEdge("B", "E", 3);
+    graph.addEdge("C", "F", 8);
+    graph.addEdge("E", "D", 17);
+    graph.addEdge("E", "F", 1);
+    graph.addEdge("D", "F", 1);
+    graph.addEdge("J", "D", 5);
+
+    // Remember here that the edges are being set from the 1st parameter edge to the 2nd.
+    // This is important to consider when testing.
+
+    const result = graph.Dijkstra("A");
+    expect(result).toEqual({
+      A: 0,
+      B: 26,
+      C: 2,
+      D: 46,
+      E: 29,
+      F: 10,
+      J: Infinity,
+    });
+  });
+
+  it("Should perform Kruskal's Algorithm for Minimum Spanning Tree", () => {
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+    graph.addVertex("D");
+    graph.addVertex("E");
+    graph.addVertex("F");
+    graph.addVertex("J");
+
+    graph.addEdge("A", "B", 3);
+    graph.addEdge("C", "A", 1);
+    graph.addEdge("A", "C", 10);
+    graph.addEdge("B", "E", 5);
+    graph.addEdge("C", "F", 4);
+
+    const result = graph.Kruskal();
+    expect(result).toEqual({
+      tree: [
+        { start: "C", end: "A", weight: 1 },
+        { start: "A", end: "B", weight: 3 },
+        { start: "C", end: "F", weight: 4 },
+        { start: "B", end: "E", weight: 5 },
+      ],
+      weight: 13,
+    });
+  });
+
+  it("Should perform Prim's Algorithm for Minimum Spanning Tree on a Directed Graph", () => {
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+    graph.addVertex("D");
+    graph.addVertex("E");
+    graph.addVertex("F");
+    graph.addVertex("J");
+
+    graph.addEdge("A", "J", 2);
+    graph.addEdge("A", "C", 3);
+    graph.addEdge("B", "F", 4);
+    graph.addEdge("C", "B", 1);
+    graph.addEdge("C", "D", 5);
+    graph.addEdge("E", "D", 2);
+    graph.addEdge("F", "E", 6);
+    graph.addEdge("F", "J", 7);
+
+    // Remember here that the edges are being set from the 1st parameter edge to the 2nd.
+    // This is important to consider when testing.
+
+    // Typically Prims Algorithm is used to find the MST on a undirected, weighted graph
+    // In this example, the graph is not connected, but a minimum spanning tree can still be found
+
+    const result = graph.Prims("A");
+    expect(result).toEqual({
+      tree: [
+        {
+          start: "A",
+          end: "J",
+          weight: 2,
+        },
+        {
+          start: "A",
+          end: "C",
+          weight: 3,
+        },
+        {
+          start: "C",
+          end: "B",
+          weight: 1,
+        },
+        {
+          start: "B",
+          end: "F",
+          weight: 4,
+        },
+        {
+          start: "C",
+          end: "D",
+          weight: 5,
+        },
+        {
+          start: "F",
+          end: "E",
+          weight: 6,
+        },
+      ],
+      minimumWeight: 21,
+    });
+  });
+
+  it("Should perform Prim's Algorithm for Minimum Spanning Tree on an Undirected Graph", () => {
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+    graph.addVertex("D");
+    graph.addVertex("E");
+    graph.addVertex("F");
+    graph.addVertex("J");
+
+    graph.addEdge("A", "B", 3, true);
+    graph.addEdge("A", "C", 1, true);
+    graph.addEdge("A", "J", 2, true);
+    graph.addEdge("B", "E", 5, true);
+    graph.addEdge("C", "F", 4, true);
+    graph.addEdge("E", "D", 10, true);
+    graph.addEdge("D", "C", 1, true);
+
+    const result = graph.Prims("A");
+    expect(result).toEqual({
+      tree: [
+        { start: "A", end: "C", weight: 1 },
+        { start: "C", end: "D", weight: 1 },
+        { start: "A", end: "J", weight: 2 },
+        { start: "A", end: "B", weight: 3 },
+        { start: "C", end: "F", weight: 4 },
+        { start: "B", end: "E", weight: 5 },
+      ],
+      minimumWeight: 16,
+    });
   });
 });
--- GraphLibrary.ts
+++ GraphLibrary.ts
@@ -2,12 +2,23 @@ import { MinPriorityQueue } from "@datastructures-js/priority-queue";
 
 type GraphType = {
   adjacencyList: { [key: string]: { vertex: string; weight: number }[] };
+  vertexList: string[];
+  edgeList: { start: string; end: string; weight: number }[];
+};
+
+type DjQueueType = {
+  priority: number;
+  element: string;
 };
 
 class Graph implements GraphType {
   adjacencyList: { [key: string]: { vertex: string; weight: number }[] };
+  vertexList: string[];
+  edgeList: { start: string; end: string; weight: number }[];
   constructor() {
     this.adjacencyList = {};
+    this.vertexList = [];
+    this.edgeList = [];
   }
 
   /**
@@ -43,13 +54,13 @@ class Graph implements GraphType {
    * @param startVertex
    * @param visited
    */
-  DFS(startVertex: string, visited: string[]) {
+  DFS(startVertex: string, visited: { [key: string]: boolean }) {
     // Mark the current vertex as visited
     visited[startVertex] = true;
 
     // For each neighbor of the current vertex, if it hasn't been visited, recursively call DFS
     this.adjacencyList[startVertex].map((neighbor) => {
-      if (!visited[neighbor.vertex]) {
+      if (!visited[Number(neighbor.vertex)]) {
         this.DFS(neighbor.vertex, visited);
       }
     });
@@ -62,7 +73,10 @@ class Graph implements GraphType {
    * @param vertex
    */
   addVertex(vertex: string) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      this.vertexList.push(vertex);
+    }
   }
 
   /**
@@ -74,17 +88,27 @@ class Graph implements GraphType {
    * @param weight
    * @returns
    */
-  addEdge(vertex1: string, vertex2: string, weight = 0) {
+  addEdge(vertex1: string, vertex2: string, weight = 0, undirected?: boolean) {
     if (vertex1 === vertex2)
       return console.log("Cannot add an edge to the same vertex");
 
-    if (
+    if (undirected) {
+      if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+        this.adjacencyList[vertex1].push({ vertex: vertex2, weight });
+        this.adjacencyList[vertex2].push({ vertex: vertex1, weight });
+        this.edgeList.push({ start: vertex1, end: vertex2, weight });
+        this.edgeList.push({ start: vertex2, end: vertex1, weight });
+      } else {
+        return console.log("One of the vertices does not exist");
+      }
+    } else if (
       this.adjacencyList[vertex1] &&
       this.adjacencyList[vertex1].map((edge) => edge.vertex !== vertex2)
     ) {
       // If the vertex is in the adjaceny list, and we haven't already added the vertex2 to the vertex1's list
       // O(1) Time complexity, preventing the possible N^2 worst case scenario
       this.adjacencyList[vertex1].push({ vertex: vertex2, weight });
+      this.edgeList.push({ start: vertex1, end: vertex2, weight });
     }
   }
 
@@ -108,29 +132,169 @@ class Graph implements GraphType {
     this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => {
       v.vertex !== vertex1;
     });
+
+    this.edgeList = this.edgeList.filter((edge) => {
+      return edge.start !== vertex1 && edge.end !== vertex2;
+    });
   }
 
   /**
-   * Dijkstra - Shortest Path Algorithm
+   * Dijkstra - Shortest Path Algorithm, finding the shortest path from one vertex to all other vertices
    * O((V+E) * log(V)) - Time Complexity
    * O(V) - Space Complexity
    * @param startVertex
    */
-  Dijkstra(startVertex: string) {}
+  Dijkstra(startVertex: string, endVertex?: string) {
+    // Comparison function for the priority queue, to sort by priority
+    const comparison = (value: DjQueueType) => {
+      return value.priority;
+    };
+
+    let heap = new MinPriorityQueue(comparison); // Min Heap, for getting the smallest edge
+    let distance: { [key: string]: number } = {}; // Distance from the start vertex to all other vertices
+
+    heap.push({ priority: 0, element: startVertex });
+    distance[startVertex] = 0;
+
+    // Initialize the distance array
+    this.vertexList.map((vertex) => {
+      if (vertex !== startVertex) {
+        distance[vertex] = Infinity; // Set all other vertices to infinity
+      }
+    });
+
+    while (heap.size() > 0) {
+      // While we have elements in the heap
+      let currentVertex = heap.pop().element;
+      // pop the element with the smallest priority
+
+      // for each neighbor of the current vertex, calculate the distance
+      this.adjacencyList[currentVertex].map((neighbor) => {
+        let candidateDistance = distance[currentVertex] + neighbor.weight;
+
+        // we check if the current distance of the node combined with the neighbor
+        // is less than the current distance of the neighbor
+        if (candidateDistance < distance[neighbor.vertex]) {
+          // update if true
+          distance[neighbor.vertex] = candidateDistance;
+          heap.push({ priority: candidateDistance, element: neighbor.vertex });
+        }
+      });
+    }
+
+    // In the case you want to see the distance from the start vertex to the end vertex
+    if (endVertex) return distance[endVertex];
+
+    // Otherwise, return the distance array, with all nodes
+    return distance;
+  }
+
+  /*
+   * Kruskals Algorithm for finding the minimum spanning tree
+   * O(E * log(E)) - Time Complexity - Looping through all edges O(E), inserting and popping from the priority queue (log E)
+   * O(E) - Space Complexity
+   */
+  Kruskal() {
+    const comparison = (value: {
+      startVertex: string;
+      vertex: string;
+      weight: number;
+    }) => {
+      return value.weight;
+    };
+    let heap = new MinPriorityQueue(comparison); // Min Heap, for getting the smallest edge
+
+    this.edgeList.map((edge) => {
+      heap.push({
+        startVertex: edge.start,
+        vertex: edge.end,
+        weight: edge.weight,
+      });
+    });
+
+    let minimumSpanningTree: { start: string; end: string; weight: number }[] =
+      [];
+    let visited: { [key: string]: boolean } = {};
+
+    this.vertexList.map((vertex) => {
+      visited[vertex] = false;
+    });
+
+    while (heap.size() > 0) {
+      let currentEdge = heap.pop();
+      if (!visited[currentEdge.startVertex] || !visited[currentEdge.vertex]) {
+        minimumSpanningTree.push({
+          start: currentEdge.startVertex,
+          end: currentEdge.vertex,
+          weight: currentEdge.weight,
+        });
+        visited[currentEdge.startVertex] = true;
+        visited[currentEdge.vertex] = true;
+      }
+    }
+
+    let totalWeight = minimumSpanningTree.reduce((acc, edge) => {
+      acc += edge.weight;
+      return acc;
+    }, 0);
+
+    return { tree: minimumSpanningTree, weight: totalWeight };
+  }
 
   /*
    *
    *
    *
    */
-  Kruskal() {}
+  Prims(startVertex: string) {
+    const comparison = (value: {
+      start: string;
+      end: string;
+      weight: number;
+    }) => {
+      return value.weight;
+    };
+    let heap = new MinPriorityQueue(comparison); // Min Heap, for getting the smallest edge connected to the current spanning tree
 
-  /*
-   *
-   *
-   *
-   */
-  Prim() {}
+    let minimumSpanningTree: { start: string; end: string; weight: number }[] =
+      [];
+
+    let visited: { [key: string]: boolean } = {}; // keep track of what vertices have been visited
+    this.vertexList.map((vertex) => {
+      // initialize the visited array to all false
+      visited[vertex] = false;
+    });
+
+    heap.push({ start: startVertex, end: startVertex, weight: 0 }); // push the start vertex to the heap
+
+    while (heap.size()) {
+      let { start, end, weight } = heap.pop();
+
+      if (!visited[end]) {
+        minimumSpanningTree.push({ start, end: end, weight });
+        visited[end] = true;
+        this.adjacencyList[end].map((neighbor) => {
+          if (!visited[neighbor.vertex]) {
+            heap.push({
+              start: end,
+              end: neighbor.vertex,
+              weight: neighbor.weight,
+            });
+          }
+        });
+      }
+    }
+
+    // Calculate the minimum weight of the minimum spanning tree
+    let minimumWeight = minimumSpanningTree.reduce((acc, edge) => {
+      acc += edge.weight;
+      return acc;
+    }, 0);
+
+    minimumSpanningTree.shift(); // remove the first element, as it is the start vertex
+
+    return { tree: minimumSpanningTree, minimumWeight: minimumWeight };
+  }
 }
 
 export default Graph;
